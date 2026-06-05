7c0be8cb6f3a241b62606a5589cb484122c4d243
LiamNoceda <jonhfeel88@gmail.com>
2026-06-05 20:28:49 +0400

    first commit

---
+++ app.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+struct Spider {
+    std::string sex{"Unknown"};
+    double x{0}, y{0};
+    int age{0};
+    std::string name{"NoName"};
+};
+
+int main() {
+    std::cout << "Wasm-Spider" << std::endl;
+
+    Spider wasm_spider{"Male", 4.5, 5.0, 1, "Ed"};
+    std::cout << "\tName this spider is " << wasm_spider.name << std::endl;
+    std::cout << "\tSex-Pol " << wasm_spider.sex << std::endl;
+    std::cout << "\tSpider x and y " << wasm_spider.x << "-" << wasm_spider.y << std::endl;
+    std::cout << "\tSpider age " << wasm_spider.age << std::endl;
+
+    return 0;
+}
